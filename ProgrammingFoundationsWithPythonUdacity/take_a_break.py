bdaf36a647d7ae45ee94059736f3fb2f6d38981c
Shaffan Mustafa <shaffan1996@gmail.com>
2017-03-19 23:31:39 -0400

    added some stuff to make it play a video every two minutes

--- ProgrammingFoundationsWithPythonUdacity/take_a_break.py
+++ ProgrammingFoundationsWithPythonUdacity/take_a_break.py
@@ -5,8 +5,7 @@ totalBreaks = 3
 breakCount = 0
 
 print("This program started on "+time.ctime())
-while(breakCount < totalBreaks)
-    time.sleep(2*60*60)
-    webbrowser.open("http://www.adweek.com/core/wp-content/uploads/sites/socialtimes/2014/10/keyboard-break.jpg")
+while(breakCount < totalBreaks):
+    time.sleep(10)
+    wb.open("http://www.adweek.com/core/wp-content/uploads/sites/socialtimes/2014/10/keyboard-break.jpg")
     breakCount = breakCount + 1
-    
