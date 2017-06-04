94f34391f5a54ab50e50b50a584e199d6240cacd
Shaffan Mustafa <shaffan1996@gmail.com>
2017-06-04 07:06:29 +0000

    made some changes to articles controller

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,9 +1,22 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles=Article.all	  
+  end 	  
   def new
   end
+
   def create
-    @articles = Article.new[params[:article])
-    @article.save 
+    @article = Article.new(article_params)
+    @article.save
     redirect_to @article
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
