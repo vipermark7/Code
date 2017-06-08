3a03cfc02a7854407c1f239b9f4552be42866ace
Shaffan Mustafa <shaffan1996@gmail.com>
2017-06-08 23:16:49 +0000

    trying to sync remote and local

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,15 +1,23 @@
 class ArticlesController < ApplicationController
 
   def index
-    @articles=Article.all	  
-  end 	  
+    @articles=Article.all
+  end
   def new
+    @post = Post.new
+    @posts=Post.index
+    @post = Post.new(params[:post].permit(:title, :text))
+    @post.save
+    redirect_to @post
   end
 
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def show
