2761061900836074939c60c5fb948f094d46d622
Shaffan Mustafa <shaffan1996@gmail.com>
2017-06-09 07:27:25 +0000

    almost to the end of the tutorial

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -3,12 +3,22 @@ class ArticlesController < ApplicationController
   def index
     @articles=Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
-    @post = Post.new
-    @posts=Post.index
-    @post = Post.new(params[:post].permit(:title, :text))
-    @post.save
-    redirect_to @post
+    @article=Article.new
+  end
+
+  def edit
+    @article = article.find(params[:id])
+    if @article.update(article_params)
+    redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   def create
@@ -20,10 +30,21 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def show
-    @article = Article.find(params[:id])
+  def update
+  @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+  
   private def article_params
     params.require(:article).permit(:title, :text)
   end
