9996e9063374eca4359c34888007557e8a702932
Shaffan Mustafa <shaffan1996@gmail.com>
2017-06-10 05:32:10 +0000

    finished with rails tutorial! :D

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,2 +1,23 @@
 class CommentsController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", only: :destory
+  def index
+    @articles = Articles.all
+  end
+
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.commetns.find(params[:id])
+    @comment.destroy
+  end
+
+  private def comment_params
+    params.require(:comment).permist(:commenter, :body)
+  end
 end
