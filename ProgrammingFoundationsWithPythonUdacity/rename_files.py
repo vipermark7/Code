b366d80724bae05d320ff6d45a7cc9da0ef10216
Shaffan Mustafa <shaffan1996@gmail.com>
2017-08-24 01:18:47 -0400

    added a few extra comments about how to proceed with web scraper

---
+++ ProgrammingFoundationsWithPythonUdacity/rename_files.py
@@ -0,0 +1,8 @@
+from os import listdir
+from os.path import isfile, join
+def rename_files() :
+    # get files names
+    # for each file name:
+        # rename file
+    file_list = os.listdir(r"C:\Users\vipermk7\Downloads\prank")
+    print(file_list)    
--- commitStripScraping.rb
+++ commitStripScraping.rb
@@ -1,25 +1,12 @@
-require 'nokogiri'
-require 'httparty'
-require 'pry'
 require 'json'
 require 'csv'
+require 'pry'
 five_newest_comics = []
 
-# we want to get stuff wih <a href> type elements
-# also img src type elements, helpful to have div id and class for this
-
-page = HTTParty.get('http://www.commitstrip.com/en/?')
-
-parse_page = Nokogiri::HTML(page)
-Pry.start(binding)
-parse_page.css('.excerpts').css('.excerpt')
+# RSS Scraping info from: https://medium.com/@josheche/parsing-an-rss-feed-in-ruby-on-rails-58b23cfb5b25
+# could also use Google+ API to get comics directly from CommitStrip's G+ page! :D 
+ 
 
 # add gem that checks to see whether page has changed and performs web scraping operations upon change  
 
-# attempting to access/download each comic even though there are multiple
-# divs all identically named "excerpt"
-parse_page.css('.excerpts').css('.excerpt').each do |div|
-  a_tag = div.at_xpath('.//div[@id="results_list"]/a')
-  puts a_tag.text
-end
 
