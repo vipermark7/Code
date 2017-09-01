9515a55cef0f5102ea540b99c979b1882e85968a
Shaffan Mustafa <shaffan1996@gmail.com>
2017-08-31 22:50:10 -0400

    implemented a function to rss_scrape that takes a link to an xmlm feed as an argument

--- commitStripScraping.rb
+++ commitStripScraping.rb
@@ -1,12 +1,26 @@
 require 'json'
-require 'csv'
 require 'pry'
-five_newest_comics = []
+require 'rss'
+require 'open-uri'
+
+rss_results = []
 
 # RSS Scraping info from: https://medium.com/@josheche/parsing-an-rss-feed-in-ruby-on-rails-58b23cfb5b25
-# could also use Google+ API to get comics directly from CommitStrip's G+ page! :D 
- 
-
 # add gem that checks to see whether page has changed and performs web scraping operations upon change  
+def comics_rss_scrape(xml_feed_string)
+    rss = RSS::Parser.parse(open(link_string).read, false).items[0..5]
 
+    rss.each do |result|
+        result = {title: result.title, date: result.pubDate, link: result.link, description: result.description}
+        rss_results.push(result)
+    end
+    return rss_results
+end
+
+# the comics() function will produce a huge data dump that we need to filter through 
+#    to get the actual comics
+# TODO: figure out how to store comic links, images, and possibly related records
+# this function can probably be applied to other websites to automate web scraping, 
+#    but they MAY have different ways to hold the data
+print(comics_rss_scrape(http://www.commitstrip.com/en/feed/))
 
