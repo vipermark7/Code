94f715b25f1652f01c4cd4a902229053c758beaa
Shaffan Mustafa <shaffan1996@gmail.com>
2017-09-01 00:13:09 -0400

    added a new method that gets comic images and saves them somewhere. will have to see where they are saved and how to use them i guess

--- commitStripScraping.rb
+++ commitStripScraping.rb
@@ -1,5 +1,6 @@
 require 'rss'
 require 'open-uri'
+require 'mechanize'
 
 # RSS Scraping info from: https://medium.com/@josheche/parsing-an-rss-feed-in-ruby-on-rails-58b23cfb5b25
 # add gem that checks to see whether page has changed and performs web scraping operations upon change  
@@ -8,10 +9,13 @@ def comics_rss_scrape(xml_feed_string)
     rss = RSS::Parser.parse(open(xml_feed_string).read, false).items[0..4]
 
     rss.each do |result|
-        result = { title: result.title,
-         date: result.pubDate,
-         link: result.link,
-         description: result.description 
+        result = 
+        { 
+           title: result.title,
+           date: result.pubDate,
+           link: result.link,
+           description: result.description, 
+           src: result.src
         }
         rss_results.push(result)
     end
@@ -20,8 +24,18 @@ end
 
 # the comics() function will produce a data dump that we need to filter through 
 #    to get the actual comics
-# TODO: figure out how to store comic links, images, and possibly related records
+
+# TODO: figure out how to store comic links, images, and possibly related records!!!!!!!!!!!
+
 # this function can probably be applied to other websites to automate web scraping, 
 #    but they MAY have different ways to hold the data
 print(comics_rss_scrape('http://www.commitstrip.com/en/feed/'))
 
+def get_comic_images(rss_results_array)
+    agent = Mechanize.new
+    rss_results_array.each do |result|
+        agent.get(result.src).save "images/#{File.basename(url)}"
+    end
+end
+
+get_comic_images(rss_results)
