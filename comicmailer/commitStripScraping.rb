b5767edaf74611abd3938665b1b253c10a9a44ee
Shaffan Mustafa <shaffan1996@gmail.com>
2017-11-07 00:45:04 -0500

    figured out how to get image from rss feed cdata tag thanks to Kevin Unrich from Buckeye Hackers!

--- comicmailer/commitStripScraping.rb
+++ comicmailer/commitStripScraping.rb
@@ -3,17 +3,19 @@ require 'open-uri'
 require 'mechanize'
 
 # RSS Scraping info from: https://medium.com/@josheche/parsing-an-rss-feed-in-ruby-on-rails-58b23cfb5b25
-# add gem that checks to see whether page has changed and performs web scraping operations upon change  
+# add gem that checks to see whether page has changed and performs web scraping operations upon change
 def comics_rss_scrape(xml_feed_string)
     rss_results = []
     rss = RSS::Parser.parse(open(xml_feed_string).read, false).items[0..4]
 
     rss.each do |result|
-        result = { 
+        result = {
           title: result.title,
           date: result.pubDate,
           link: result.link,
-          description: result.description
+          description: result.description,
+          image_link: URI.extract(result.content_encoded)
+
           # XPath: //item/content-encoded returns all content-encoded returns all
           # content-encoded stuff in document
         }
@@ -21,7 +23,7 @@ def comics_rss_scrape(xml_feed_string)
     end
     rss_results.each do |el|
       puts el
-    end  
+    end
     return rss_results
 end
 
@@ -29,7 +31,7 @@ end
 # TODO: figure out how to store comic links, images, and possibly related records!!!!!!!!!!!
 # file systems are preferred to databases in regards to image storage, good to use with open source compression software!
 
-# this function can probably be applied to other websites to automate web scraping, 
+# this function can probably be applied to other websites to automate web scraping,
 #    but they MAY have different ways to hold the data
 comics_rss_scrape('http://www.commitstrip.com/en/feed/')
 
