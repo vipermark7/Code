5ddd59bc52f48267e5e15abd8dd2ab9e370a8357
Shaffan Mustafa <shaffan1996@gmail.com>
2018-01-03 15:05:16 -0500

    restarting django tutorial

--- ProgrammingFoundationsWithPythonUdacity/rename_files.py
+++
@@ -1,8 +0,0 @@
-from os import listdir
-from os.path import isfile, join
-def rename_files() :
-    # get files names
-    # for each file name:
-        # rename file
-    file_list = os.listdir(r"C:\Users\vipermk7\Downloads\prank")
-    print(file_list)    
