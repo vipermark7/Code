9a6d14406642c9662da564daab9e645824b8f887
Shaffan Mustafa <shaffan1996@gmail.com>
2018-12-19 10:48:18 -0500

    added code snippet to get all contacts

--- ContactReviewer/App.js
+++ ContactReviewer/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Contacts from 'react-native-contacts';
+
+Contacts.getAll((err, contacts) => {
+    if (err) throw err;
+	     // contacts returned
+	    console.log(contacts)
+    })
 export default class App extends React.Component {
   render() {
     return (
