686005e1bd7029151c24e055c4012abf47d312af
Shaffan <shaffan1996@gmail.com>
2019-01-24 19:46:15 -0500

    app.js

--- ContactReviewer/App.js
+++ ContactReviewer/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
 import Contacts from 'react-native-contacts';
+import styled from 'styled-components/native';
+import { TabNavigator } from 'react-navigation';
 
-Contacts.getAll((err, contacts) => {
-    if (err) throw err;
-	     // contacts returned
-	    console.log(contacts)
-    })
+//Contacts.getAll((err, contacts) => {
+    //if (err) throw err;
+	     //// contacts returned
+	    //console.log(contacts)
+    //})
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text> Hi! Do you ever add new contacts to your phone only to forget about them after not talking to them for a while?
-          This app is built to help you stop doing that! It will give you an optional quiz about your recently added contacts, while
-          asking you if you've kept in touch with your older contacts as well. 
-        </Text>
-      </View>
+      <Container>
+        <TextStyled> 
+          <Text>
+			      Hi! Do you ever add new contacts to your phone only to forget about them after not talking to them for a while?
+          </Text>
+          <Text>
+            This app is built to help you stop doing that! It will give you an optional quiz about your recently added contacts, while
+            asking you if you've kept in touch with your older contacts as well. 
+          </Text>
+        </TextStyled>
+      </Container>
     );
   }
 }
@@ -24,8 +30,48 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingHorizontal: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+const Container = styled.View`
+justifyContent: center;
+alignItems: center;
+margin: 300px;
+`;
+const TextStyled = styled.Text`
+fontSize: 20;
+textAlign: center;
+margin: 10px;
+`
+
+
+// tab navigator example from Tracy Lee on Medium
+import ContactsList from './A';
+import ContactsQuiz from './Angular';
+const mainNavigator = TabNavigator({
+  Home: {
+    screen: javaScriptJeopardyRn,
+    path: ''
+  },
+  Angular: {
+    screen: Angular,
+    path: 'angular'
+  },
+  React: {
+    screen: ReactJS,
+    path: 'react'
+  },
+  Ember: {
+    screen: Ember,
+    path: 'ember'
+  },
+  Vue: {
+    screen: Vue,
+    path: 'vue'
+  }
+});
+AppRegistry.registerComponent('javaScriptJeopardyRn', () => mainNavigator);
