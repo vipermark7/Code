0b2dc2198b43ea8c0bcb71158a6811f2e56f2d93
Shaffan Mustafa <shaffan1996@gmail.com>
2019-07-06 01:05:37 -0400

    adding poker kata

---
+++ poker_kata.py
@@ -0,0 +1,42 @@
+# Suits are denoted C, D, H, and S
+# Values are denoted 2, 3, 4, 5, 6, 7, 8, 9, T, J, Q, K, A
+
+# Straight flush: 5 cards of the same suit with consecutive values. 
+# Ranked by the highest card in the hand.
+
+
+
+ # if set(faces) has 5 elements then you either have a bunch of crap or a
+ # straight. You can tell the difference by comparing the max/min of # the  set, which will give you the spread.
+
+ # if set(faces) is two you either have 4 + 1 or 3 +2
+
+ # if set(suits) is 1 you have a flush
+ 
+ def poker(hand): 
+     flush,
+     straight,
+     highCard, 
+     pair,  
+     twoPairs, 
+     threeOfaKind, 
+     straight,
+     Flush,
+     fullHouse, 
+     fourOfaKind,
+     straightFlush = False
+     
+     # Flush
+     if len(set(suits)) == 1:
+        print("Flush")
+        flush = True 
+        
+      # Straight 
+      if
+      
+      # Straight Flish 
+      if straight == True and flush == True:
+          straightFlush = True
+      
+                
+ 
