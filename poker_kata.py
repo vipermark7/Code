d49e76fdd4fa410698a03d62264019385d545db8
Shaffan <shaffan1996@gmail.com>
2019-07-14 19:24:59 -0400

    working on read hands

--- poker_kata.py
+++ poker_kata.py
@@ -19,28 +19,32 @@ cardValues = (2, 3, 4, 5, 6, 7, 8 ,9, 'T', 'J', 'Q', 'K', 'A')
 cardSuits = ('C', 'H', 'D', 'S')
 
 def Repeat(x): # function that adds numbers that are repeated to a list
-_size = len(x)
-repeated = []
-for i in range(_size):
-    k = i + 1
-    for j in range(k, _size):
-        if x[i] == x[j] and x[i] not in repeated:
-            repeated.append(x[i])
-            return repeated
+    _size = len(x)
+    repeated = []
+    for i in range(_size):
+        k = i + 1
+        for j in range(k, _size):
+            if x[i] == x[j] and x[i] not in repeated:
+                repeated.append(x[i])
+                return repeated
 
 def read_hands():
     pokerhand = open("C:/Users/Shaffan/pokerhand.txt").read()
     sep_hands = pokerhand.splitlines()
     sep_hands_wo_black = []
-    sep_hands_wo_black_white = []
+    sep_hands_wo_bw = []
     for hand in sep_hands:
         hand[7:44]
     for hand in sep_hands:
         sep_hands_wo_black.append(hand[7:44])
     for h in sep_hands_wo_black:
-        sep_hands_wo_black_white.append(h.replace(" White: ", " "))
-    for h in sep_hands_wo_black_white:
+        sep_hands_wo_bw.append(h.replace(" White: ", " "))
+    for h in sep_hands_wo_bw:
         print(h)
+# len("2H 3D 5S 9C KD") = 14 (first half of the first hand)
+
+        
+    
         
         
 def rank_hand(hand): # array of cards
@@ -65,17 +69,17 @@ def rank_hand(hand): # array of cards
     returnVal = -2 # default return value
 
     # Pair: 2 of the 5 cards in the hand have the same value/rank
-    if len(Repeat(hand)) == 4
+    if len(Repeat(hand)) == 4:
         pair = True
         returnVal = 1
 
     #Two pairs: two pairs in a hand
-    elif len(repeated(hand)) == 3
+    elif len(repeated(hand)) == 3:
         twoPairs = True
         returnVal = 2
 
     # Three of a Kind: Three of the cards in the hand have the same value.
-    elif len(repeated(hand)) == 3
+    elif len(repeated(hand)) == 3:
         threeOfaKind = True
         returnVal = 3
     # Flush: Hand contains 5 cards of the same suit. Hands which are both
@@ -86,7 +90,7 @@ def rank_hand(hand): # array of cards
 
     # Straight: Hand contains 5 cards with consecutive values. Hands which both
     # contain a straight are ranked by their highest card.
-    if (max(values) - min(values) == 4) and len(set(values)) == 5
+    if (max(values) - min(values) == 4) and len(set(values)) == 5:
         returnVal = 5
 
     # Full House - needs a Pair and Three of a Kind
@@ -99,8 +103,8 @@ def rank_hand(hand): # array of cards
 
     # Straight Flush
     if straight == True and flush == True:
-            straightFlush = True
-            returnVal = 8
-        else:
-            returnVal = 0
-            return returnVal
+        straightFlush = True
+        returnVal = 8
+    else:
+        returnVal = 0
+        return returnVal
