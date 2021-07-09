843343b0efc547e0ae10a702afaca8d93856319c
Shaffan Mustafa <shaffan1996@gmail.com>
2021-07-08 23:57:20 -0400

    Update cd-database.lisp

--- lispstuff/cd-database.lisp
+++ lispstuff/cd-database.lisp
@@ -1,9 +1,9 @@
+(defvar *db* nil)
+
 (defun make-cd (title artist rating ripped)
   (list :title title :artist artist :rating rating :ripped ripped))
 
-(defvar *db* nil)
-
-(defun add-record (cd) 
+(defun add-record (cd)
   (push cd *db*))
 
 (defun dump-db ()
@@ -19,17 +19,17 @@
   (make-cd
    (prompt-read "Title")
    (prompt-read "Artist")
-   (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
-   (y-or-n-p "Ripped [y/n]: ")))
+   (or 0 (parse-integer (prompt-read "Rating"))) 
+   (prompt-read (y-or-n-p "Ripped? (y/n)"))))
 
 (defun add-cds ()
   (loop (add-record (prompt-for-cd))
-      (if (not (y-or-n-p "Another? [y/n]: ")) (return))))
+	(if (not (y-or-n-p "Another [y/n]:")) (return))))
 
 (defun save-db (filename)
   (with-open-file (out filename
-                   :direction :output
-                   :if-exists :supersede)
+                       :direction :output
+                       :if-exists :supersede)
     (with-standard-io-syntax
       (print *db* out))))
 
@@ -38,7 +38,37 @@
     (with-standard-io-syntax
       (setf *db* (read in)))))
 
-(defun select-by-artist (artist)
-  (remove-if-not
-   #'(lambda (cd) (equal (getf cd :artist) artist))
-   *db*))
+(defun select (selector-fn)
+  (remove-if-not selector-fn *db*))
+
+(defun where (&key title artist rating (ripped nil ripped-p))
+  #'(lambda (cd)
+      (and
+       (if title    (equal (getf cd :title)  title)  t)
+       (if artist   (equal (getf cd :artist) artist) t)
+       (if rating   (equal (getf cd :rating) rating) t)
+       (if ripped-p (equal (getf cd :ripped) ripped) t))))
+
+(defun update (selector-fn &key title artist rating (ripped nil ripped-p))
+  (setf *db*
+        (mapcar
+         #'(lambda (row)
+             (when (funcall selector-fn row)
+               (if title    (setf (getf row :title) title))
+               (if artist   (setf (getf row :artist) artist))
+               (if rating   (setf (getf row :rating) rating))
+               (if ripped-p (setf (getf row :ripped) ripped)))
+             row) *db*)))
+
+(defun delete-rows (selector-fn)
+  (setf *db* (remove-if selector-fn *db*)))
+
+(defun make-comparison-expr (field value)
+  `(equal (getf cd ,field) ,value))
+
+(defun make-comparisons-list (fields)
+  (loop while fields
+        collecting (make-comparison-expr (pop fields) (pop fields))))
+
+(defmacro where (&rest clauses)
+  `#'(lambda (cd) (and ,@(make-comparisons-list clauses))))
