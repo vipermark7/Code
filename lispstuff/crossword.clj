02e03591154e180d80e623606de9070216602996
Shaffan Mustafa <shaffan1996@gmail.com>
2022-01-01 03:16:32 -0500

    Add files via upload

---
+++ lispstuff/crossword.clj
@@ -0,0 +1,30 @@
+(ns crossword)
+
+;; (mapv (fn [x] (str/split  (x 0) #" ")) data) 
+;; when we have a vector of vectors, each vector
+;; containing only a big string with all the letters
+;; in the row
+
+(def data 
+  [["A" "O" "T" "D" "L" "R" "O" "W"] 
+   ["L" "C" "B" "M" "U" "M" "L" "U"] 
+   ["D" "R" "U" "J" "D" "B" "L" "J"] 
+   ["P" "A" "Z" "H" "Z" "Z" "E" "F"] 
+   ["B" "C" "Z" "E" "L" "F" "H" "W"] 
+   ["R" "K" "U" "L" "V" "P" "P" "G"] 
+   ["A" "L" "B" "L" "P" "O" "P" "Q"] 
+   ["B" "E" "M" "O" "P" "P" "J" "Y"]])
+
+(defn letter-column [letter-idx] 
+  (map (fn [x] (x letter-idx)) data))
+
+(defn above-letter [letter-idx  ])
+
+(defn below-letter)
+
+
+
+
+
+
+
