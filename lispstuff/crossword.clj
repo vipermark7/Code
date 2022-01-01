ca26abc0ca6ed12d9166935f82b8faced229c354
Shaffan Mustafa <shaffan1996@gmail.com>
2022-01-01 03:34:06 -0500

    Update crossword.clj

--- lispstuff/crossword.clj
+++ lispstuff/crossword.clj
@@ -1,26 +1,33 @@
 (ns crossword)
 
+(ns crossword
+  (:require [clojure/string :as str])
+
 ;; (mapv (fn [x] (str/split  (x 0) #" ")) data) 
 ;; when we have a vector of vectors, each vector
 ;; containing only a big string with all the letters
 ;; in the row
 
-(def data 
-  [["A" "O" "T" "D" "L" "R" "O" "W"] 
-   ["L" "C" "B" "M" "U" "M" "L" "U"] 
-   ["D" "R" "U" "J" "D" "B" "L" "J"] 
-   ["P" "A" "Z" "H" "Z" "Z" "E" "F"] 
-   ["B" "C" "Z" "E" "L" "F" "H" "W"] 
-   ["R" "K" "U" "L" "V" "P" "P" "G"] 
-   ["A" "L" "B" "L" "P" "O" "P" "Q"] 
-   ["B" "E" "M" "O" "P" "P" "J" "Y"]])
+  (def data
+    [["A" "O" "T" "D" "L" "R" "O" "W"]
+     ["L" "C" "B" "M" "U" "M" "L" "U"]
+     ["D" "R" "U" "J" "D" "B" "L" "J"]
+     ["P" "A" "Z" "H" "Z" "Z" "E" "F"]
+     ["B" "C" "Z" "E" "L" "F" "H" "W"]
+     ["R" "K" "U" "L" "V" "P" "P" "G"]
+     ["A" "L" "B" "L" "P" "O" "P" "Q"]
+     ["B" "E" "M" "O" "P" "P" "J" "Y"]]))
 
 (defn letter-column [letter-idx] 
-  (map (fn [x] (x letter-idx)) data))
+  (mapv #(% letter-idx) data))
 
-(defn above-letter [letter-idx  ])
+(defn above-letter [letter-idx]
+  (subvec (letter-column letter-idx) 
+          0 letter-idx))
 
-(defn below-letter)
+(defn below-letter [letter-idx]
+  (subvec (letter-column letter-idx) 
+          letter-idx (.length letter-column)))
 
 
 
