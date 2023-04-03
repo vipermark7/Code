38f6bc7ffe3cfc5fdea4f432f95739a5d35222ee
Shaffan <shaffan1996@gmail.com>
2023-04-02 23:40:06 -0400

    keeping stuff up to date

--- lispstuff/way.rkt
+++ lispstuff/way.rkt
@@ -1,3 +1,4 @@
+;; way: a GUI program that displays and edits your system's PATH environment variable
 ; way: a GUI program that displays and edits your system's PATH environment variable
 
 #lang racket
@@ -32,14 +33,21 @@
                    [width 700]
                    [height 500]))
 
+(define path-entries (new list-box%
+                         [label "Path Items"]
+                         [parent frame]
+                         [choices (string-split path ":")]))
 
 (define edit (new button%
                   [label "Edit"]
+                  [parent frame]))
+
                   [parent frame]
                   [callback edit-path-els]))
-                
+
 (define add (new button%
                   [label "Add"]
+                  [parent frame]))
                   [parent frame]
                   ;; this should make the GUI element path-entries change value
                   ;; but not change the value of the actual env var
@@ -52,8 +60,4 @@
 (define (apply-changes) (putenv ))
 
 (define delete (new button%
-                  [label "Delete"]
-                  [parent frame]))
-
-; Display GUI
-(send frame show #t)
+                  [label "Delete"]))
